2215ccd722d10bc3a416db536a34edf2c216e21d
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-01-16 21:00:44 +0800

    update

--- src/test/java/MonitorTest.java
+++ src/test/java/MonitorTest.java
@@ -1,9 +1,13 @@
 import accumulate.http.HttpRequest;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeBodyPart;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeMultipart;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,7 +19,7 @@ public class MonitorTest {
     public void test() throws Exception {
         HttpRequest httpRequest = new HttpRequest();
 
-        String [] origin = new String[]{"Vulture", "rocket", "","carrier", "Micius", "payment", "virtualReality", "Online", "Economy", "Support", "Authentication", "Targeted", "Interests", "Like", "SmallCat", "DoubleFirstClass", "keystore", "SlowMist", "wayish", "SNZPool", "HashQuark", "InfStones", "Cultural", "Majorpower", "Energy", "Traffic", "Connectivity", "Vehicle", "Flight", "Overseas", "Confidence", "Alternative", "Cooldown", "Merry", "Cynosure", "Stash", "water-node", "Depth", "song", "hopeless", "crossborder", "Kio", "craftsmanship", "domestically", "Initative", "Strikegold", "waiter", "soulgaze", "hotnormal", "ChineseDream", "Craftsman", "Independent", "Sing", "Fater", "Skills", "Zero", "Superstition", "TextBookstyle", "official", "fighter", "intelligence", "Economic", "freestyle", "invisible", "whitefragility", "Store", "Driving", "Super1", "Unicorn", "apparent", "Peak1", "Milkshake", "Parents", "Habitable", "Innovation", "Stream", "Destroyer", "ormal", "Regional", "Tiered", "Ambitious", "E", "Subcenter", "Opportunity", "Management", "Modern", "Super", "Reform", "Pilot", "Sharing", "Production", "Series", "Peak", "Couch", "Secret", "Calorie", "Naughty", "Star", "Status", "Proof", "Friend", "Personality", "platon.node.3", "platon.node.6", "platon.node.4", "platon.node.2", "platon.node.1", "platon.node.5", "platon.node.7"};
+        String [] origin = new String[]{"Vulture", "rocket", "", "carrier", "Micius", "payment", "virtualReality", "Online", "Economy", "Support", "Authentication", "Targeted", "Interests", "Like", "SmallCat", "DoubleFirstClass", "keystore", "SlowMist", "wayish", "SNZPool", "HashQuark", "InfStones", "Cultural", "Majorpower", "Energy", "Traffic", "Connectivity", "Vehicle", "Flight", "Overseas", "Confidence", "Alternative", "Cooldown", "Merry", "Cynosure", "Stash", "water-node", "Depth", "song", "hopeless", "crossborder", "Kio", "craftsmanship", "domestically", "Initative", "Strikegold", "waiter", "soulgaze", "hotnormal", "ChineseDream", "Craftsman", "Independent", "Sing", "Fater", "Skills", "Zero", "Superstition", "TextBookstyle", "official", "fighter", "intelligence", "Economic", "freestyle", "invisible", "whitefragility", "Store", "Driving", "Super1", "Unicorn", "apparent", "Peak1", "Milkshake", "Parents", "Habitable", "Innovation", "Stream", "Destroyer", "ormal", "Regional", "Tiered", "Ambitious", "E", "Subcenter", "Opportunity", "Management", "Modern", "Super", "Reform", "Pilot", "Sharing", "Production", "Series", "Peak", "Couch", "Secret", "Calorie", "Naughty", "Star", "Status", "Proof", "Friend", "Personality", "platon.node.3", "platon.node.6", "platon.node.4", "platon.node.2", "platon.node.1", "platon.node.5", "platon.node.7"};
         String url = "https://platscan.platon.network/browser-server/staking/aliveStakingList";
         String param = "{\"pageNo\": 1, \"pageSize\": 150, \"key\": \"\", \"queryStatus\": \"all\"}";
 
@@ -32,17 +36,17 @@ public class MonitorTest {
                 nodeNames.add(nodeName);
             }
 
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 20; i++) {
                 if (!nodeNames.get(i).equals(origin[i])) {
                     System.out.println("发生变化了.");
                     System.out.println(nodeNames.subList(0, 10));
-                    FileUtils.writeStringToFile(new File("changeinfo.txt"), String.valueOf(nodeNames.subList(0, 10)), "UTF-8");
-
                     flag = false;
                     break;
                 }
             }
         }
+
+
 
     }
 }
